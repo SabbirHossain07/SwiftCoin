263a1a0f4c7d74e5787aaa25b449c53cb26d0201
SabbirHossain07 <sabbirsopnil07@gmail.com>
2023-06-15 22:10:35 +0600

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Sopnil Sohan on 15/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
